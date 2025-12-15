5626be4c7c0f99cf11d47cc9d22ae582962d93f8
SilicDev <83308290+SilicDev@users.noreply.github.com>
2025-12-15 21:36:19 +0100

    Add Quantity setup

--- Sonic3AIR.cs
+++ Sonic3AIR.cs
@@ -18,9 +18,9 @@ class Sonic3AIR : SimpleTCPPack<SimpleTCPServerConnector>
             List<Effect> effects =
             [
                 new("Give Ring", "AddRing")
-                    { Price = 1, Description = "Give the player a ring." },
+                    { Price = 1, Description = "Give the player a ring.", Quantity = 99 },
                 new("Take Ring", "TakeRing")
-                    { Price = 1, Description = "Take a ring from the player" },
+                    { Price = 1, Description = "Take a ring from the player", Quantity = 99 },
                 new("Change to Sonic", "CharSonic")
                     { Price = 5, Description = "Change the character to Sonic." },
                 new("Change to Tails", "CharTails")
